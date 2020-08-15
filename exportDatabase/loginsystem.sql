dcbe5831f767b1d586998aa08133c88aec0eac19
Arpita2327 <47477257+Arpita2327@users.noreply.github.com>
2020-08-15 22:09:48 +0600

    Update loginsystem.sql

--- exportDatabase/loginsystem.sql
+++ exportDatabase/loginsystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 14, 2020 at 04:41 PM
+-- Generation Time: Aug 15, 2020 at 06:06 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.2.32
 
@@ -21,12 +21,41 @@ SET time_zone = "+00:00";
 -- Database: `loginsystem`
 --
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `tid` int(11) NOT NULL,
+  `task` text NOT NULL,
+  `useid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Dumping data for table `tasks`
 --
 
 INSERT INTO `tasks` (`tid`, `task`, `useid`) VALUES
-(3, 'rrrrrrrr', 5);
+(2, 'fffffffff', 5),
+(3, 'rrrrrrrr', 5),
+(4, 'gggg', 4),
+(27, 'lllll', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `create_datetime` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
@@ -35,6 +64,49 @@ INSERT INTO `tasks` (`tid`, `task`, `useid`) VALUES
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `create_datetime`) VALUES
 (4, 'a', 'aroy6852@gmail.com', 'fcea920f7412b5da7be0cf42b8c93759', '2020-08-12 18:49:38'),
 (5, 'arpita2327', 'arpita35-2327@diu.edu.bd', '25d55ad283aa400af464c76d713c07ad', '2020-08-13 12:25:28');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`tid`),
+  ADD KEY `id` (`useid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `tid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD CONSTRAINT `id` FOREIGN KEY (`useid`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
